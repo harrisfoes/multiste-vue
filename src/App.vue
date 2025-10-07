6df536e2128ee5f5038d3e19b594a1d66859bb1c
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-10-07 19:50:38 +0900

    first page mobile

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import bg_mobile from '@/assets/images/bg-sidebar-mobile.svg'
+import StepItem from './components/StepItem.vue';
+</script>
 
 <template>
-  <main>
-    <div class="form-card">
-      <h2>Personal info</h2>
-      <p>
-        Please provide your name, email address, and phone number.
-      </p>
-      <form>
-        <label>
-          Name
-        </label>
-        <div>
-          <input placeholder="e.g. Stephen King" />
-        </div>
-        <label>
-          Email Address
-        </label>
-        <div>
-          <input type="email" placeholder="e.g. stephenking@lorem.com" />
-        </div>
-        <label>
-          Phone Number
-        </label>
-        <div>
-          <input placeholder="e.g. 1 234 567 890" />
-        </div>
-        <button>Next Step</button>
-      </form>
-    </div>
-  </main>
+  <div class="container">
+    <main>
+      <div class="bg-sidebar">
+        <img :src="bg_mobile" class="bg-mobile" alt="mobile background">
+      </div>
+      <div class="step-counter">
+        <StepItem v-for="n in 4" :key="n" :step-num="n" />
+      </div>
+      <div class="form-card">
+        <h2>Personal info</h2>
+        <p>
+          Please provide your name, email address, and phone number.
+        </p>
+        <form>
+          <label>
+            Name
+          </label>
+          <div>
+            <input placeholder="e.g. Stephen King" />
+          </div>
+          <label>
+            Email Address
+          </label>
+          <div>
+            <input type="email" placeholder="e.g. stephenking@lorem.com" />
+          </div>
+          <label>
+            Phone Number
+          </label>
+          <div>
+            <input placeholder="e.g. 1 234 567 890" />
+          </div>
+          <button class="next-step">Next Step</button>
+        </form>
+      </div>
+    </main>
+    <footer>
+    </footer>
+  </div>
 </template>
 
 <style>
@@ -52,17 +65,89 @@
 body {
   background-color: var(--bg-color);
   font-family: Ubuntu, sans-serif;
+  margin: 0 auto;
+  color: var(--grey);
+}
+
+.container {
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.bg-mobile {
+  width: 100%;
+  position: absolute;
+  z-index: -2;
+}
+
+.step-counter {
+  display: flex;
+  gap: 1rem;
+
+  position: absolute;
+  top: 48px;
+  left: 50%;
+  transform: translateX(-50%)
 }
 
 h2 {
   color: var(--denim);
+  font-size: 24px;
+}
+
+p {
+  font-size: 16px;
+  line-height: 25px;
+}
+
+label {
+  font-size: 12px;
+  color: var(--denim);
+}
+
+input {
+  border: 1px solid var(--border-color);
+  height: 40px;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0.25rem 0rem;
+  padding: 0px 12px;
+  border-radius: 4px;
 }
 
 .form-card {
   background-color: var(--white);
   border-radius: 10px;
-  margin: 2rem 2rem;
+  margin: 0rem 2rem;
+  margin-top: 7.5rem;
   padding: 2rem 2rem;
   border: 1px solid var(--border-color);
+  box-shadow: 0px 25px 40px -20px rgba(0, 0, 0, 0.1)
+}
+
+button {
+  background: none;
+  border: none;
+  margin: 0;
+  font: inherit;
+  text-align: center;
+  color: white;
+}
+
+.next-step {
+  position: absolute;
+  background-color: var(--denim);
+  height: 40px;
+  width: 97px;
+  border-radius: 4px;
+  inset: auto 32px 32px auto;
+}
+
+footer {
+  background-color: var(--white);
+  height: 6rem;
 }
 </style>
---
+++ src/components/StepItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{ stepNum: number }>()
+</script>
+
+<template>
+  <span class="steps">{{ stepNum }}</span>
+</template>
+
+<style scoped>
+.steps {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid var(--white);
+  color: var(--white);
+  font-weight: bold;
+  height: 33px;
+  width: 33px;
+  border-radius: 50%;
+}
+</style>
